9717e93fa83c42fc9dd8dccd468de1d59bd440c5
caradguadarrama <caradguadarrama@gmai.com>
2026-06-04 20:45:00 -0600

    updates

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,12 @@
-console.log('Hello, World!');
+// wellcome text
+const logo = document.querySelector('.logo');
 
+const lang = ["Hello World", "Hola Mundo", "ハロー・ワールド"];
 
+let i = 0;
+
+setInterval(() => {
+    i = (i + 1) % lang.length;
+    logo.textContent = lang[i];
+}, 2000);
 
